199dbfdcdd0657fcb4c73f58810f7d88d947d693
Eugene Park <epark215@gmail.com>
2023-01-06 04:43:01 +0000

    Completed arrayChange

--- arrayChange/index.js
+++ arrayChange/index.js
@@ -2,17 +2,14 @@ function solution(inputArray) {
   let change = 0;
   for (let i = 0; i < inputArray.length; i++) {
     if (inputArray[i] > inputArray[i + 1]) {
-      inputArray[i] = (inputArray[i + 1] - inputArray[i] + 1);
-      change += (inputArray[i + 1] - inputArray[i] + 1);
+      change += ((inputArray[i] - inputArray[i + 1]) + 1);
+      inputArray[i + 1] += ((inputArray[i] - inputArray[i + 1]) + 1);
     } else if (inputArray[i] === inputArray[i + 1]) {
       inputArray[i + 1] += 1;
       change += 1;
-    } else if (inputArray[i + 1] - inputArray[i] !== 1 && inputArray[i] < inputArray[i + 1]) {
-      inputArray[i] = (inputArray[i + 1] - inputArray[i] + 1);
-      change += (inputArray[i + 1] - inputArray[i] + 1);
     }
   } return change;
 }
 
-const newInputArray = [1, 1, 1];
+const newInputArray = [-1000, 0, -2, 0];
 solution(newInputArray);
