98bf99444132930769507fc9ed0e46138c8fe07e
Eugene Park <epark215@gmail.com>
2023-01-07 01:10:17 +0000

    Completed areEqaullyStrong

--- areEquallyStrong/index.js
+++ areEquallyStrong/index.js
@@ -1,7 +1,9 @@
 function solution(yL, yR, fL, fR) {
   const your = yL + yR;
   const friend = fL + fR;
-  if (your === friend) {
+  if (your !== friend) {
+    return false;
+  } else if (yL === fL || yR === fL || yL === fR || yR === fR) {
     return true;
   } else {
     return false;
@@ -10,7 +12,7 @@ function solution(yL, yR, fL, fR) {
 
 const yourLeft = 10;
 const yourRight = 15;
-const friendsLeft = 15;
-const friendsRight = 10;
+const friendsLeft = 5;
+const friendsRight = 20;
 
 solution(yourLeft, yourRight, friendsLeft, friendsRight);
