8842887a55a20a4f2aa7269b4e49d4202cf61f82
Eugene Park <epark215@gmail.com>
2023-01-08 23:21:26 +0000

    Completed arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/index.js
+++ arrayMaximalAdjacentDifference/index.js
@@ -1,10 +1,12 @@
 function solution(inputArray) {
-  let maxDifference = 0;
+  let maxDifference = inputArray[0] - inputArray[1];
   for (let i = 0; i < inputArray.length; i++) {
-    if ((inputArray[i] - inputArray[i + 1]) > maxDifference) {
+    if ((inputArray[i] - inputArray[i + 1]) > (inputArray[i + 1] - inputArray[i]) && (inputArray[i] - inputArray[i + 1]) > maxDifference) {
       maxDifference = (inputArray[i] - inputArray[i + 1]);
+    } else if ((inputArray[i + 1] - inputArray[i]) > (inputArray[i] - inputArray[i + 1]) && (inputArray[i + 1] - inputArray[i]) > maxDifference) {
+      maxDifference = (inputArray[i + 1] - inputArray[i]);
     }
   } return maxDifference;
 }
 
-(solution([2, 4, 1, 0]));
+solution([-1, 4, 10, 3, -2]);
