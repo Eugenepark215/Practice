83901f8dcfaae388f539cb1cb761b73782bec3d5
Eugene Park <epark215@gmail.com>
2023-02-01 04:56:49 +0000

    refactored using regex

--- variableName/index.js
+++ variableName/index.js
@@ -1,10 +1,5 @@
 function solution(name) {
-  for (let i = 0; i < name.length; i++) {
-    if (Number.isInteger(parseInt(name[0])) || name.includes('-') || name[0] === ' ' || name.includes('[') || name.includes(' ')) {
-      return false;
-    }
-  }
-  return true;
+  return /^[a-z_]\w*$/i.test(name);
 }
 
 solution('123');
