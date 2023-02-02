4488962591f98c3848bd48c23764e34b8873045e
Eugene Park <epark215@gmail.com>
2023-02-02 08:02:12 +0000

    Applied tuple annotation

--- typeScriptArray/Tuples/index.ts
+++ typeScriptArray/Tuples/index.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-unused-vars
-let favoriteCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
+let favoriteCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
