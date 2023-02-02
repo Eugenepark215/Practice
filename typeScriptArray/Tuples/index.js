c4b1a730a82557a4925528bb596dcc8261b3e6b2
Eugene Park <epark215@gmail.com>
2023-02-02 08:05:14 +0000

    Added tuple annotation

--- typeScriptArray/Tuples/index.js
+++ typeScriptArray/Tuples/index.js
@@ -1,2 +1,4 @@
+'use strict';
+exports.__esModule = true;
 // eslint-disable-next-line no-unused-vars
 var favoriteCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
--- typeScriptArray/Tuples/index.ts
+++ typeScriptArray/Tuples/index.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line no-unused-vars
 let favoriteCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
+export { }
