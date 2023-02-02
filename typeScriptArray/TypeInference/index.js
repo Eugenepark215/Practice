623b6714bee442b36386424c0601d45c1032776f
Eugene Park <epark215@gmail.com>
2023-02-02 08:20:51 +0000

    initial commit TypeInference

---
+++ typeScriptArray/TypeInference/index.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-unused-vars
+var dogTup = ['dog', 'brown fur', 'curly tail', 'sad eyes'];
---
+++ typeScriptArray/TypeInference/index.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-unused-vars
+let dogTup: [string, string, string, string] = ['dog', 'brown fur', 'curly tail', 'sad eyes'];
