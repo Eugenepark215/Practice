cbf4fbfbc8af074bc263494926aa310c2deef01d
Eugene Park <epark215@gmail.com>
2023-02-02 08:00:43 +0000

    Initial commit Tuple

---
+++ typeScriptArray/Tuples/index.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-unused-vars
+var favoriteCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
---
+++ typeScriptArray/Tuples/index.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-unused-vars
+let favoriteCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
