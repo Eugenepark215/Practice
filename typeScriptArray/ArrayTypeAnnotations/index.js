3e89c4f55eda5a440070eedb214a9daa5530ad61
Eugene Park <epark215@gmail.com>
2023-02-02 06:52:12 +0000

    Initial commit ArrayTypeAnnotations

---
+++ typeScriptArray/ArrayTypeAnnotations/index.js
@@ -0,0 +1,4 @@
+// let bestNumbers = [7, 77, 4];
+// let bestLunches = ['chicken soup', 'non-chicken soup'];
+// let bestBreakfasts = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+// let bestBooleans = [true, false];
