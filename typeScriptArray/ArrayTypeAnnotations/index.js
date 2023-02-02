93bcbb666c46890ca7c0e2938aa259b9418bfcc9
Eugene Park <epark215@gmail.com>
2023-02-02 07:28:59 +0000

    Completed ArrayTypeAnnotation

--- typeScriptArray/ArrayTypeAnnotations/index.js
+++ typeScriptArray/ArrayTypeAnnotations/index.js
@@ -1,4 +1,10 @@
-// let bestNumbers = [7, 77, 4];
-// let bestLunches = ['chicken soup', 'non-chicken soup'];
-// let bestBreakfasts = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
-// let bestBooleans = [true, false];
+'use strict';
+exports.__esModule = true;
+// eslint-disable-next-line no-unused-vars
+var bestNumbers = [7, 77, 4];
+// eslint-disable-next-line no-unused-vars
+var bestLunches = ['chicken soup', 'non-chicken soup'];
+// eslint-disable-next-line no-unused-vars
+var bestBreakfasts = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+// eslint-disable-next-line no-unused-vars
+var bestBooleans = [true, false];
--- typeScriptArray/ArrayTypeAnnotations/index.ts
+++ typeScriptArray/ArrayTypeAnnotations/index.ts
@@ -1,4 +1,9 @@
-// let bestNumbers = [7, 77, 4];
-// let bestLunches = ['chicken soup', 'non-chicken soup'];
-// let bestBreakfasts = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
-// let bestBooleans = [true, false];
+// eslint-disable-next-line no-unused-vars
+let bestNumbers: number[] = [7, 77, 4];
+// eslint-disable-next-line no-unused-vars
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+// eslint-disable-next-line no-unused-vars
+let bestBreakfasts: string[] = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+// eslint-disable-next-line no-unused-vars
+let bestBooleans: boolean[] = [true, false];
+export { }
