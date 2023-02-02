b31328ddaa6f623c030c718dd5a79ab0e58674d2
Eugene Park <epark215@gmail.com>
2023-02-02 07:57:27 +0000

    Completed MultiDimensionalArray

--- typeScriptArray/MultiDimensional/index.js
+++ typeScriptArray/MultiDimensional/index.js
@@ -1,8 +1,14 @@
-// let bestNumbers: number[] = [7, 77, 4];
-// let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
-// let bestBreakfasts: string[] = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
-// let bestBooleans: boolean[] = [true, false];
-// Multidimensional Arrays:
-// let bestMealPlan = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
-// let bestBooleansTwice = [bestBooleans, bestBooleans];
-// let numbersMulti = [[[1], [2, 3]], [[7], bestNumbers]];
+// eslint-disable-next-line no-unused-vars
+var bestNumbers = [7, 77, 4];
+// eslint-disable-next-line no-unused-vars
+var bestLunches = ['chicken soup', 'non-chicken soup'];
+// eslint-disable-next-line no-unused-vars
+var bestBreakfasts = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+// eslint-disable-next-line no-unused-vars
+var bestBooleans = [true, false];
+// eslint-disable-next-line no-unused-vars
+var bestMealPlan = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
+// eslint-disable-next-line no-unused-vars
+var bestBooleansTwice = [bestBooleans, bestBooleans];
+// eslint-disable-next-line no-unused-vars
+var numbersMulti = [[[1], [2, 3]], [[7], bestNumbers]];
--- typeScriptArray/MultiDimensional/index.ts
+++ typeScriptArray/MultiDimensional/index.ts
@@ -1,9 +1,15 @@
-// let bestNumbers: number[] = [7, 77, 4];
-// let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
-// let bestBreakfasts: string[] = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
-// let bestBooleans: boolean[] = [true, false];
+// eslint-disable-next-line no-unused-vars
+let bestNumbers: number[] = [7, 77, 4];
+// eslint-disable-next-line no-unused-vars
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+// eslint-disable-next-line no-unused-vars
+let bestBreakfasts: string[] = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+// eslint-disable-next-line no-unused-vars
+let bestBooleans: boolean[] = [true, false];
 
-// Multidimensional Arrays:
-// let bestMealPlan = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
-// let bestBooleansTwice = [bestBooleans, bestBooleans];
-// let numbersMulti = [[[1], [2, 3]], [[7], bestNumbers]];
+// eslint-disable-next-line no-unused-vars
+let bestMealPlan: string[][] = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
+// eslint-disable-next-line no-unused-vars
+let bestBooleansTwice: boolean[][] = [bestBooleans, bestBooleans];
+// eslint-disable-next-line no-unused-vars
+let numbersMulti: number[][][] = [[[1], [2, 3]], [[7], bestNumbers]];
