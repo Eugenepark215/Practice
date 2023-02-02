2969a000f83f05966f1c1dde74879aba6431564b
Eugene Park <epark215@gmail.com>
2023-02-02 08:18:06 +0000

    Completed tuples

--- typeScriptArray/Tuples/index.js
+++ typeScriptArray/Tuples/index.js
@@ -2,3 +2,5 @@
 exports.__esModule = true;
 // eslint-disable-next-line no-unused-vars
 var favoriteCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
+// eslint-disable-next-line no-unused-vars
+favoriteCoordinates = [17, 45, 'N', 142, 30, 'E'];
