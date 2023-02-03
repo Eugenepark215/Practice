8dc9629d69773fd775b0a7a6869774e2ed79a5f4
Eugene Park <epark215@gmail.com>
2023-02-03 06:19:24 +0000

    Added type annotation

--- typeScriptArray/restParameters/index.ts
+++ typeScriptArray/restParameters/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-function addPower(p, ...numsToAdd) {
+function addPower(p: number, ...numsToAdd: number[]): number {
   let answer = 0;
   for (let i = 0; i < numsToAdd.length; i++) {
     answer += numsToAdd[i] ** p;
