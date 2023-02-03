037becdeee7031a11d47bf470ecd289a66c77c77
Eugene Park <epark215@gmail.com>
2023-02-03 06:06:29 +0000

    Complete typeInference

--- typeScriptArray/TypeInference/index.js
+++ typeScriptArray/TypeInference/index.js
@@ -1,2 +1,7 @@
+'use strict';
+exports.__esModule = true;
 // eslint-disable-next-line no-unused-vars
 var dogTup = ['dog', 'brown fur', 'curly tail', 'sad eyes'];
+// eslint-disable-next-line no-unused-vars
+var myArr = dogTup.concat(dogTup);
+myArr[50] = 'not a dog';
