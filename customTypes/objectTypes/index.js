85141ff3587952051620bb16037471dc9625ab2d
Eugene Park <epark215@gmail.com>
2023-02-08 03:02:38 +0000

    Inital commit objectTypes

---
+++ customTypes/objectTypes/index.js
@@ -0,0 +1,25 @@
+'use strict';
+exports.__esModule = true;
+// eslint-disable-next-line no-unused-vars
+function sayHappyBirthdayWithObject(personObject) {
+  var output = '';
+  output += 'Happy Birthday ' +
+        personObject.name + '! ';
+  output += 'You are now ' +
+        personObject.age + ' years old! ';
+  output += 'Your birthday wish was to receive ' +
+        personObject.giftWish +
+        '. And guess what? You will ';
+  if (!personObject.success) {
+    output += 'not ';
+  }
+  output += 'receive it! \n';
+  // eslint-disable-next-line no-console
+  console.log(output);
+}
+var birthdayBabies = [
+  { name: 'Liam', age: 0, giftWish: 'karate skills', success: false },
+  { name: 'Olivia', age: 0, giftWish: 'a bright future', success: true },
+  { name: 'Ava', age: 0, giftWish: '$0.25', success: true }
+];
+birthdayBabies.forEach(sayHappyBirthdayWithObject);
---
+++ customTypes/objectTypes/index.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+function sayHappyBirthdayWithObject(personObject) {
+  let output = '';
+  output += 'Happy Birthday '
+    + personObject.name + '! ';
+  output += 'You are now '
+    + personObject.age + ' years old! ';
+  output += 'Your birthday wish was to receive '
+    + personObject.giftWish
+    + '. And guess what? You will ';
+  if (!personObject.success) {
+    output += 'not ';
+  }
+  output += 'receive it! \n';
+  // eslint-disable-next-line no-console
+  console.log(output);
+}
+
+let birthdayBabies = [
+  { name: 'Liam', age: 0, giftWish: 'karate skills', success: false },
+  { name: 'Olivia', age: 0, giftWish: 'a bright future', success: true },
+  { name: 'Ava', age: 0, giftWish: '$0.25', success: true }
+];
+
+birthdayBabies.forEach(sayHappyBirthdayWithObject);
+
+export { }
