fa393313ac387722e36056327b92071a09ea76d7
Eugene Park <epark215@gmail.com>
2023-02-08 02:02:18 +0000

    Initial commit enums

---
+++ customTypes/enums/index.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+var petOnSale = 'chinchilla';
+// eslint-disable-next-line no-unused-vars
+var ordersArray = [
+  ['rat', 2],
+  ['chinchilla', 1],
+  ['hamster', 2],
+  ['chinchilla', 50]
+];
