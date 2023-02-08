d00d898f7e24e33ffcddf0a60f3378d7e4f12c87
Eugene Park <epark215@gmail.com>
2023-02-08 03:12:14 +0000

    Included type annotation to parameter personObject and variable birthdayBabies

--- customTypes/objectTypes/index.ts
+++ customTypes/objectTypes/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-function sayHappyBirthdayWithObject(personObject) {
+function sayHappyBirthdayWithObject(personObject: { name: string, age: number, giftWish: string, success: boolean }) {
   let output = '';
   output += 'Happy Birthday '
     + personObject.name + '! ';
@@ -16,7 +16,7 @@ function sayHappyBirthdayWithObject(personObject) {
   console.log(output);
 }
 
-let birthdayBabies = [
+let birthdayBabies: { name: string, age: number, giftWish: string, success: boolean }[] = [
   { name: 'Liam', age: 0, giftWish: 'karate skills', success: false },
   { name: 'Olivia', age: 0, giftWish: 'a bright future', success: true },
   { name: 'Ava', age: 0, giftWish: '$0.25', success: true }
