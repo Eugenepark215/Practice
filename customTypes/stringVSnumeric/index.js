0903a4ed176d54e0a702a6b628b6ea70ad0be7b1
Eugene Park <epark215@gmail.com>
2023-02-08 02:29:41 +0000

    Initial commit

---
+++ customTypes/stringVSnumeric/index.js
@@ -0,0 +1,11 @@
+'use strict';
+exports.__esModule = true;
+// eslint-disable-next-line no-unused-vars
+var petOnSale = 'chinchilla';
+// eslint-disable-next-line no-unused-vars
+var ordersArray = [
+  ['rat', 2],
+  ['chinchilla', 1],
+  ['hamster', 2],
+  ['chinchilla', 50]
+];
---
+++ customTypes/stringVSnumeric/index.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+let petOnSale = 'chinchilla';
+// eslint-disable-next-line no-unused-vars
+let ordersArray = [
+  ['rat', 2],
+  ['chinchilla', 1],
+  ['hamster', 2],
+  ['chinchilla', 50]
+];
+
+export { }
