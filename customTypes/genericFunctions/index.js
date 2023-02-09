207652fb6cdf803390db8d48c07764de4def72ee
Eugene Park <epark215@gmail.com>
2023-02-09 06:45:18 +0000

    Initial commit genericFunctions

---
+++ customTypes/genericFunctions/index.js
@@ -0,0 +1,14 @@
+'use strict';
+exports.__esModule = true;
+// eslint-disable-next-line no-unused-vars
+function getFilledArray(value, n) {
+  return Array(n).fill(value);
+}
+// eslint-disable-next-line no-unused-vars
+var stringArray;
+// eslint-disable-next-line no-unused-vars
+var numberArray;
+// eslint-disable-next-line no-unused-vars
+var personArray;
+// eslint-disable-next-line no-unused-vars
+var coordinateArray;
---
+++ customTypes/genericFunctions/index.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+function getFilledArray<T>(value: T, n: number): T[] {
+  return Array(n).fill(value);
+}
+// eslint-disable-next-line no-unused-vars
+let stringArray: string[];
+// eslint-disable-next-line no-unused-vars
+let numberArray: number[];
+// eslint-disable-next-line no-unused-vars
+let personArray: { name: string, age: number }[];
+// eslint-disable-next-line no-unused-vars
+let coordinateArray: [number, number][];
+
+export { }
