26cb1d1b6c6498858353170a6ce96cd1edc59b07
Eugene Park <epark215@gmail.com>
2023-02-09 03:17:42 +0000

    Created type Coord and refactored type annotation

--- customTypes/typeAliases/index.ts
+++ customTypes/typeAliases/index.ts
@@ -1,6 +1,8 @@
+type Coord = [number, number, string, number, number, string]
+
 // eslint-disable-next-line no-unused-vars
-let codecademyCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
+let codecademyCoordinates: Coord = [40, 43.2, 'N', 73, 59.8, 'W'];
 // eslint-disable-next-line no-unused-vars
-let bermudaTCoordinates: [number, number, string, number, number, string] = [25, 0, 'N', 71, 0, 'W'];
+let bermudaTCoordinates: Coord = [25, 0, 'N', 71, 0, 'W'];
 
 export { }
