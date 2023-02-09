0ac8a523c261a789f152526d42fc44af6d121a9e
Eugene Park <epark215@gmail.com>
2023-02-09 06:04:10 +0000

    Added type annotation to variables using generic types

--- customTypes/genericTypes/index.js
+++ customTypes/genericTypes/index.js
@@ -1,34 +1,34 @@
-// type Human = { name: string, job: string };
-// type Dog = { name: string, tailWagSpeed: number };
-// type Family<T> = {
-//   parents: [T, T], mate: T, children: T[]
-// };
-// Do not change the code above this line.
-// Provide type annotations for the variables below:
-// let theFamily = {
-//   parents: [3, 4], mate: 9, children: [5, 30, 121]
-// };
-// let someFamily = {
-//   parents: [true, true], mate: false,
-//   children: [false, false, true, true]
-// };
-// let aFamily = {
-//   parents: [
-//     { name: 'Mom', job: 'software engineer' },
-//     { name: 'Dad', job: 'coding engineer' }
-//   ],
-//   mate: { name: 'Matesky', job: 'engineering coder' },
-//   children: [{ name: 'Babesky', job: 'none' }]
-// };
-// let anotherFamily = {
-//   parents: [
-//     { name: 'Momo', tailWagSpeed: 3 },
-//     { name: 'Dado', tailWagSpeed: 100 }
-//   ],
-//   mate: { name: 'Cheems', tailWagSpeed: 7 },
-//   children: [
-//     { name: 'Puppin', tailWagSpeed: 0.001 },
-//     { name: 'Puppenaut', tailWagSpeed: 0.0001 },
-//     { name: 'Puppenator', tailWagSpeed: 0.01 }
-//   ]
-// };
+'use strict';
+exports.__esModule = true;
+// eslint-disable-next-line no-unused-vars
+var theFamily = {
+  parents: [3, 4], mate: 9, children: [5, 30, 121]
+};
+// eslint-disable-next-line no-unused-vars
+var someFamily = {
+  parents: [true, true],
+  mate: false,
+  children: [false, false, true, true]
+};
+// eslint-disable-next-line no-unused-vars
+var aFamily = {
+  parents: [
+    { name: 'Mom', job: 'software engineer' },
+    { name: 'Dad', job: 'coding engineer' }
+  ],
+  mate: { name: 'Matesky', job: 'engineering coder' },
+  children: [{ name: 'Babesky', job: 'none' }]
+};
+// eslint-disable-next-line no-unused-vars
+var anotherFamily = {
+  parents: [
+    { name: 'Momo', tailWagSpeed: 3 },
+    { name: 'Dado', tailWagSpeed: 100 }
+  ],
+  mate: { name: 'Cheems', tailWagSpeed: 7 },
+  children: [
+    { name: 'Puppin', tailWagSpeed: 0.001 },
+    { name: 'Puppenaut', tailWagSpeed: 0.0001 },
+    { name: 'Puppenator', tailWagSpeed: 0.01 }
+  ]
+};
