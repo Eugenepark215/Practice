84f7ee0b0fb8057e0930886a832243a4b07fc563
Eugene Park <epark215@gmail.com>
2023-02-09 03:11:28 +0000

    Initial commit typeAliases

---
+++ customTypes/typeAliases/index.js
@@ -0,0 +1,6 @@
+'use strict';
+exports.__esModule = true;
+// eslint-disable-next-line no-unused-vars
+var codecademyCoordinates = [40, 43.2, 'N', 73, 59.8, 'W'];
+// eslint-disable-next-line no-unused-vars
+var bermudaTCoordinates = [25, 0, 'N', 71, 0, 'W'];
---
+++ customTypes/typeAliases/index.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+let codecademyCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
+// eslint-disable-next-line no-unused-vars
+let bermudaTCoordinates: [number, number, string, number, number, string] = [25, 0, 'N', 71, 0, 'W'];
+
+export { }
