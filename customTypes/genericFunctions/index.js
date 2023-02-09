090072a2039ee5e51d3003b531ee68a6861fd847
Eugene Park <epark215@gmail.com>
2023-02-09 07:10:54 +0000

    Utilized generic functions to assign values to variables

--- customTypes/genericFunctions/index.js
+++ customTypes/genericFunctions/index.js
@@ -1,14 +1,17 @@
 'use strict';
 exports.__esModule = true;
-// eslint-disable-next-line no-unused-vars
 function getFilledArray(value, n) {
   return Array(n).fill(value);
 }
-// eslint-disable-next-line no-unused-vars
 var stringArray;
-// eslint-disable-next-line no-unused-vars
 var numberArray;
-// eslint-disable-next-line no-unused-vars
 var personArray;
-// eslint-disable-next-line no-unused-vars
 var coordinateArray;
+// eslint-disable-next-line no-unused-vars
+stringArray = getFilledArray('hi', 6);
+// eslint-disable-next-line no-unused-vars
+numberArray = getFilledArray(9, 6);
+// eslint-disable-next-line no-unused-vars
+personArray = getFilledArray({ name: 'J. Dean', age: 24 }, 6);
+// eslint-disable-next-line no-unused-vars
+coordinateArray = getFilledArray([3, 4], 6);
--- customTypes/genericFunctions/index.ts
+++ customTypes/genericFunctions/index.ts
@@ -1,14 +1,18 @@
-// eslint-disable-next-line no-unused-vars
 function getFilledArray<T>(value: T, n: number): T[] {
   return Array(n).fill(value);
 }
-// eslint-disable-next-line no-unused-vars
+
 let stringArray: string[];
-// eslint-disable-next-line no-unused-vars
 let numberArray: number[];
-// eslint-disable-next-line no-unused-vars
 let personArray: { name: string, age: number }[];
-// eslint-disable-next-line no-unused-vars
 let coordinateArray: [number, number][];
 
+// eslint-disable-next-line no-unused-vars
+stringArray = getFilledArray<string>('hi', 6);
+// eslint-disable-next-line no-unused-vars
+numberArray = getFilledArray<number>(9, 6);
+// eslint-disable-next-line no-unused-vars
+personArray = getFilledArray<{ name: string, age: number }>({ name: 'J. Dean', age: 24 }, 6);
+// eslint-disable-next-line no-unused-vars
+coordinateArray = getFilledArray<[number, number]>([3, 4], 6);
 export { }
